1900579e827c0ad9510c01ec0e7ae6ae51cc314c
Harley Armstrong <hjarmstr@uwaterloo.ca>
2016-05-24 13:25:43 -0400

    Clean up old lab1, and add state transition logic

--- Lab2/src/states.h
+++ Lab2/src/states.h
@@ -5,17 +5,21 @@
 
 #define MAX_STATES 20
 #define INPUT_SIZE UCHAR_MAX
-
-
-struct state createFSM(unsigned char numStates, unsigned char initialState);
-void resetFSM(struct state);
-void setTransition(unsigned char val, struct state from, struct state to);
+#define STATE_SIZE_T unsigned char
+#define INPUT_TYPE unsigned char
 
 struct state
 {
-	unsigned char numStates;
-	unsigned char initialState;
-	unsigned char transition[MAX_STATES][INPUT_SIZE];
+	STATE_SIZE_T numStates;
+	STATE_SIZE_T initialState;
+	STATE_SIZE_T transition[MAX_STATES][INPUT_SIZE];
+	STATE_SIZE_T currState;
 };
+
+void createFSM(struct state me, STATE_SIZE_T numStates, STATE_SIZE_T initialState);
+void resetFSM(struct state me);
+void setTransition(struct state me, INPUT_TYPE *val, STATE_SIZE_T from, STATE_SIZE_T to);
+STATE_SIZE_T transition(struct state me, INPUT_TYPE val); // Returns state switched to... NULL == BAD
+int isNull(struct state me);
 
 #endif

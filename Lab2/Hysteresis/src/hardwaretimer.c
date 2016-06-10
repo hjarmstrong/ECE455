77f30c26ca990156a12bc0b27e5d8d2ca8725843
Harley Armstrong <hjarmstr@uwaterloo.ca>
2016-06-10 10:10:41 -0400

    Hystersis style fixes

--- Lab2/Hysteresis/src/hardwaretimer.c
+++ Lab2/Hysteresis/src/hardwaretimer.c
@@ -13,12 +13,13 @@ void TIMER0_IRQHandler(void)
 	
 	__disable_irq();
 	
-	LPC_TIM0->TCR = 0; // Diable Debounce timer
+	// Diable Debounce timer
+	LPC_TIM0->TCR = 0; 
 
 	if(input.currState == Debounce)
 	{
-		// Assert button is low (high logically but pressing it makes it go low)
-		timerInit(12599999); // start dash timer
+		 // start dash timer
+		timerInit(12599999);
 	}
 	else if (input.currState == DotDebounce)
 	{
@@ -42,21 +43,19 @@ void TIMER0_IRQHandler(void)
 
 void timerInit(int cycles)
 {	
-  LPC_TIM0->TCR = (1<<1);
+	// Reset The Timer
+	LPC_TIM0->TCR = (1<<1);
   
-	// The TC ticks: 1 -> 0 -> 1, then interupts, so the prescale 
-  // Timer must count half of one second, which at 25Mhz, is: 
-  // (25 - 1) M /2 = 12499999 prescale ticks. because 1 tick
-  // occurs when TC ticks.
-  LPC_TIM0->PR = cycles;
-	//LPC_TIM0->PR = 0x21;
+	// The PC counts cycles, and ticks the TC when
+	// it reache sthe values stored in PR
+	LPC_TIM0->PR = cycles;
 
-  LPC_TIM0->MR0 = 1;
+	LPC_TIM0->MR0 = 1;
   
-	// Set TC to 0 when it reaches 1, and interupt when TC reaches MR0
-  LPC_TIM0->MCR = 1 | (1 << 1);
+	// Set TC to 0 when it reaches 1, and interupt
+	// when TC reaches MR0
+	LPC_TIM0->MCR = 1 | (1 << 1);
 
-
-  // Finally, we enable the Timer
-  LPC_TIM0->TCR = 1;
+  // Finally, we enable the Timer, and clear the reset bit
+	LPC_TIM0->TCR = 1;
 }
